147a85c341c54f25684580c43f2a39e38a93fb0d
Juan Carlos Silva <juancarlossilva@MacBook-Pro-de-Juan.local>
2021-10-06 21:32:30 -0300

    vamos

--- nextv/pages/index.js
+++ nextv/pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import fire from "../config/fire-config"
 import { useEffect, useRef, useState } from 'react';
+import ReactPlayer from "react-player"
+import VideoPlayer from 'react-native-web-video/Video';
 
 export default function Home() {
   
@@ -225,8 +227,9 @@ useEffect(()=>{
  doc.onSnapshot(
   (docSnapshot) => {
 
-        console.log(docSnapshot.exists)
-        console.log(docSnapshot.data().link)
+        if(docSnapshot.data().link){
+          setSrc(docSnapshot.data().link)
+        }
   }, err => {
     console.log(`Encountered error: ${err}`);
   },
@@ -254,13 +257,57 @@ const myCallback = () => {
 }
 
 const ref = useRef();
+const CallFace = () => {
+  setTimeout(Mudo,8000)
+}
+function Mudo(){
+  console.log(ref)
+  console.log(ref.current.player)
+  console.log(ref.current.player.player)
+  
+  ref.current.player.player.unmute()
+  ref.current.player.handlePlay()
 
+}
 
-const Video = () =>{
+const Video = () => {
   return(
-    <video onEnded={() => myCallback()} autoPlay="autoplay" controls muted>    
-        <source ref={ref} src={src} type="video/mp4"/>
-      </video>
+    <VideoPlayer source={{uri: src}}   // Can be a URL or a local file.
+       ref={(ref) => {
+         this.player = ref
+       }}                                      // Store reference
+       rate={1.0}                              // 0 is paused, 1 is normal.
+       volume={1.0}                            // 0 is muted, 1 is normal.
+       muted={false}                           // Mutes the audio entirely.
+       paused={false}                          // Pauses playback entirely.
+       resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
+       repeat={true}                           // Repeat forever.
+       playInBackground={false}                // Audio continues to play when app entering background.
+       playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+       ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
+       progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+       onLoadStart={this.loadStart}            // Callback when video starts to load
+       onLoad={this.setDuration}               // Callback when video loads
+       onProgress={this.setTime}               // Callback every ~250ms with currentTime
+       onEnd={this.onEnd}                      // Callback when playback finishes
+       onError={this.videoError}               // Callback when video cannot be loaded
+       onBuffer={this.onBuffer}                // Callback when remote video is buffering
+       onTimedMetadata={this.onTimedMetadata}  />
+  )
+}
+
+const Video2 = () =>{
+  return(
+    <ReactPlayer
+        url={src}
+        onEnded={() => myCallback()}
+        onReady={()=>CallFace()}
+        controls
+        muted
+        playing
+        ref={ref}
+      />
+   
   )
 }
 
---
+++ nextv/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    resolve: {
+        alias: {
+            'react-native': 'react-native-web',
+            
+            'Video': 'react-native-web-video/src/Video',
+        }
+    }
+}
