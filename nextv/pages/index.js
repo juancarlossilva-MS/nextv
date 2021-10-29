4fb05b5fd995780509609e5514c6d6cef1609107
juancarlossrp-ms <jjcarlos853@gmail.com>
2021-10-29 17:04:02 -0300

    vamooos

--- nextv/pages/index.js
+++ nextv/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import fire from "../config/fire-config"
 import { useEffect, useRef, useState } from 'react';
-import {List, TextField,Button, InputLabel, ListItem } from '@mui/material';
+import {List, FormGroup,Grid,FormControlLabel,Checkbox, TextField,Button, InputLabel, ListItem } from '@mui/material';
 
 export default function Home() {
   
@@ -74,59 +74,79 @@ useEffect(()=>{
 function salvarLive(){
   console.log(link)
 }
+function salvarVideos(){
+  console.log(link)
+}
 const flexContainer = {
   flexDirection: 'row',
   padding: 0,
   
 };
-const LinkLive = () =>{
-  return(<div >
-          <InputLabel >Link da Transmissao ao vivo</InputLabel>
-          <InputLabel>obs: deixe vazio para offline</InputLabel>
-        
-        <TextField id="outlined-basic"  onChange={e => setLink(e.target.value) } value={link}  fullWidth label="Link Live" variant="standard" />
-        <Button onClick={salvarLive}>Salvar</Button>
-     </div>)
-}
 
-const VideosLista = () =>{
-  return(<div>
-    <List style={flexContainer}>
-    {videos.map(x => {
-      
-      return(
-       
-        <ListItem key={x.id}>
-            
-                      <video key={x.id} width={300} controls height={300}><source type="video/mp4" src={"https://btgnews.com.br/videos/"+x.id+"?to=crop&r=256"} /> </video>
-                      <ul>
-                      {locais.map(l => {
-                        return(
-                                <li key={l}>{l} - {x.data()[l] !== undefined && "SELECTED"  }</li>
-                                )
-                                
-                              })}
-                      </ul>
-                     
 
-                      
-          </ListItem>)
-        })}
-        </List>
-   </div>)
-}
+
+
 
   return (
-    <div className={styles.container}>
-      <div style={{height: `200px`,
-            overflow: "scroll",}}>
+    <div style={{padding:30}}>
+      <Grid container >
+            <Grid item xs={12} >
+                <InputLabel >Link da Transmissao ao vivo</InputLabel>
+                <InputLabel>obs: deixe vazio para offline</InputLabel>
+              
+              <TextField id="outlined-basic"  onChange={e => setLink(e.target.value) } value={link}  fullWidth label="Link Live" variant="standard" />
+          </Grid>
+          <div style={{paddingTop:30}}>
+        
+        <Button   variant="contained" color="success" onClick={salvarLive}>Salvar Live</Button>
+  
+        </div> 
+    </Grid>
+    <Grid container>
+          
+        
+              {videos.map(x => {
+                
+                return(
+                 <Grid item x={2} >
+                  <ListItem key={x.id}>
+                      
+                                <video key={x.id} width={300} controls height={300}><source type="video/mp4" src={"https://btgnews.com.br/videos/"+x.id+"?to=crop&r=256"} /> </video>
+                                <FormGroup>
+                                {locais.map(l => {
+                                  
+                                     if( x.data()[l] !== undefined){
+                                        return(
+                                          <FormControlLabel control={<Checkbox   defaultChecked  />} label={l} />
+                                      )
+                                     }else{
+                                            return(
+                                                                                
+                                              <FormControlLabel control={<Checkbox  />} label={l} />
+                                              
+                                                )
+                                     }
+                                     
 
-         <LinkLive/>
-      </div>
-      <div>
+                                  
+                                          
+                                        })}
+                               </FormGroup>
+                              
 
-         <VideosLista/>
-      </div>
+                                
+                    </ListItem>
+                  </Grid> 
+                    
+                    )
+                  })}
+                  
+      </Grid>    
+      <div style={{padding:30}}>
+        
+      <Button   variant="contained" color="success" onClick={salvarVideos}>Salvar Videos</Button>
+
+      </div>             
      
      </div>
   )
